b470233d630bd367f50dc5743c82d82876897a4f
礼刚 曹 <1296933@qq.com>
2023-03-18 22:56:23 +0800

    练习了一个界面框架

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import hometest from '@/views/home.vue'
+import hometest from '@/views/homtest.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +21,7 @@ const router = createRouter({
     {
       path:'/hometest',
       name: 'hometest',
-      component:()=>import('@/views/home.vue')
+      component:()=>import('@/views/homtest.vue')
     }
   ]
 })
